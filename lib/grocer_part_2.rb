d5ad0757762875961736dc48fc4db20445c6466d
Matt Green <magreen118@gmail.com>
2020-06-16 12:58:34 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -54,7 +54,7 @@ def checkout(cart, coupons)
     counter += 1
   end
   if grand_total > 100.00
-    grand_total = (grand_total 0.90).round(2)
+    grand_total = (grand_total * 0.90).round(2)
   end
   return grand_total
   
